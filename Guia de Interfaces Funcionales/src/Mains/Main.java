f65ea815b0fe7172dc899b0d242d917b69c0bd99
Nicolas Buggenthin <buggenthin.15@gmail.com>
2024-09-28 22:01:56 -0300

    Ejercicios Resueltos de Interfaces Funcionales

--- Guia de Interfaces Funcionales/src/Mains/Main.java
+++ Guia de Interfaces Funcionales/src/Mains/Main.java
@@ -14,15 +14,16 @@ public class Main {
 		
 		//--------Objetos------//
 		
-		Persona personaA = new Persona("Arturo", 45, 74, 180);
-		Persona personaB = new Persona("Carolina", 25, 50, 145);
+		Persona personaA = new Persona("Arturo", 15, 74, 2.10);
+		Persona personaB = new Persona("Carolina", 25, 50, 1.45);
+		Persona personaC = new Persona ("Bart", 10, 40, 1.10);
 		
-		Rectangulo rectyA = new Rectangulo(5, 5);
-		Rectangulo rectyB = new Rectangulo(9,14);
+		Rectangulo rectyA = new Rectangulo(15, 30);
+		Rectangulo rectyB = new Rectangulo(5,5);
 		
 		// Comparator <T> Lambda
 		//Crear un comparador que compare dos personas por altura.
-		
+		System.out.println("-----------COMPARATOR<T>-----------");
 		System.out.println("------COMPARADOR ALTURA-----------");
 		
 		Comparator<Persona> comparadorAltura = (perA, perB) -> Double.compare(perA.getEstatura(), perB.getEstatura());
@@ -56,6 +57,8 @@ public class Main {
 
 		
 	//Function <T , R>
+	System.out.println("-----------FUNCTION<T, R>-----------");
+	
 	//Crear un función que reciba un rectángulo y retorne su área
 	System.out.println("------Retorno de Area Rectangulo-------");
 	Function<Rectangulo, Double> retornarAreaRec = (recA) -> recA.getBase() * recA.getAltura();
@@ -98,7 +101,81 @@ public class Main {
 	System.out.println(retornarTrip.apply(personaB));
 
 	
+	//Predicate <T>
+	System.out.println("-----------PREDICATE<T>-----------");
+
+	//Crear un predicado que evalúe si una persona mide más de dos metros
+	System.out.println("-------Evaluar si mide mas de dos metros ----------");
+	Predicate <Persona> personaMasDos = a -> a.getEstatura() > 2.00;
+	if (personaMasDos.test(personaA)) {
+		System.out.println("Mide mas de dos metros.");
+	} else {
+		System.out.println("Mide menos de dos metros");
+	}
 	
+	//Crear un predicado que evalúe si la persona no mide más de dos metros 
+	System.out.println("-------Evaluar si mide menos de dos metros ----------");
+	Predicate <Persona> personaMenosDos = a -> a.getEstatura() < 2.00;
+	if (personaMenosDos.test(personaB)) {
+		System.out.println("Mide menos de dos metros");
+	}else {
+		System.out.println("Mide mas de dos metros");
+	}
 	
+	//Crear un predicado que evalúe si es mayor de edad 
+	System.out.println("------Evaluar si es mayor de edad------");
+	Predicate <Persona> personaMayor = a -> a.getEdad() >18;
+	if(personaMayor.test(personaA)) {
+		System.out.println("Es mayor de edad");
+	}else {
+		System.out.println("Es menor de edad");
+	}
+	
+	//Crear un predicado que evalúe si una persona es mayor de edad o mide más de 2 metros
+	System.out.println("------Evaluar si es mayor de edad o mide mas de 2 metros -------");
+	Predicate <Persona> personaMayorOMasDos = a -> a.getEdad() > 18 || a.getEstatura() > 2.00;
+	if(personaMayorOMasDos.test(personaC)) {
+	    System.out.println("Es mayor de edad o mide más de 2 metros");
+	} else {
+		System.out.println("No cumple con ninguna de las condiciones");
+	}
+	
+	//Crear un predicado que evalúe si un rectángulo es un cuadrado
+	System.out.println("--------Evaluar si un rectangulo o es un cuadrado----------");
+	Predicate<Rectangulo> esUnCuadrado = a -> a.getAltura() == a.getBase();
+	if (esUnCuadrado.test(rectyA)) {
+		System.out.println("No es un rectangulo, es un CUADRADO");
+	} else {
+		System.out.println("Es un RECTANGULO");
+	}
+	
+	//Consumer <T>
+	System.out.println("-----------CONSUMER<T>-----------");
+
+	//Realizar un consumidor que reciba una persona y muestre su edad por pantalla. 
+	System.out.println("-----Muestra de Edad -------");
+	Consumer<Persona> mostrarEdad = a -> System.out.println("Tiene: " +a.getEdad() + " años.");
+	mostrarEdad.accept(personaA);
+	
+	//Realizar un consumidor que incremente la edad de una persona en 3. 
+	System.out.println("------Incrementador de Edad x 3 ----------");
+	Consumer<Persona> incremenTres = a -> System.out.println("Edad incrementada por tres: " + a.getEdad()*3);
+	incremenTres.accept(personaB);
+	
+	//Realizar un consumidor que incremente la edad en 3 de una persona y luego muestre	su edad. 
+	System.out.println("-----Muestra de Edad y Edad Triplicada -------");
+	Consumer <Persona> mostrarEdadIncrementada = a -> {
+		System.out.println("Edad actual: " + a.getEdad());
+		System.out.println("Edad Triplicada: " + (a.getEdad()*3));
+	};
+	mostrarEdadIncrementada.accept(personaC);
+	
+	//Realizar un consumidor que transforme un rectángulo en un cuadrado, haciendo que su base pase a valer lo que su altura.
+	System.out.println("---------De Rectangulo a Cuadrado-------");
+	Consumer <Rectangulo> recACuadrado = a -> a.setBase(a.getAltura());
+	System.out.println("Base: " + rectyA.getBase() + " Altura:" + rectyA.getAltura());
+	recACuadrado.accept(rectyA);
+	System.out.println("Base: " + rectyA.getBase() + " Altura:" + rectyA.getAltura());
+
 	}
 }
